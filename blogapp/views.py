4ebbb46b91a67a1629872b8e6ffb5beec5922144
LGH <517478314@qq.com>
2017-11-14 10:37:15 +0800

    第三次提交

--- blogapp/views.py
+++ blogapp/views.py
@@ -10,7 +10,7 @@ def index(request):
     return render(request, 'blog/index.html', context={'post_list':post_list})
 def detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
-
+    # 记得在顶部引入 markdown 模块
     post.body = markdown.markdown(post.body,extensions=[
                                      'markdown.extensions.extra',
                                      'markdown.extensions.codehilite',
